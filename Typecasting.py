c6a7606ee6b12d4925e3a973e7aef61fa11344d0
jaydeyhs <jaydeyhs@gmail.com>
2026-02-02 20:21:15 -0600

    First Commit

---
+++ Typecasting.py
@@ -0,0 +1,10 @@
+# Typecasting = The process of converting a variable from one data type to another.
+
+name = "Alice"
+age = 25
+gpa = 3.8
+is_student = True
+
+name = bool(name)
+
+print(name)
---
+++ Variables-practice.py
@@ -0,0 +1,22 @@
+# A variable contains a value that can be changed during program execution. 
+
+# Strings are sequences of characters used to represent text.
+first_name = "John"
+food = "Pizza"
+email = "john@example.com"
+
+# Integers are whole numbers without a decimal point.
+age = 30
+quantity = 5
+num_of_students = 30
+
+# Floats are numbers that contain a decimal point.
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Booleans represent one of two values: True or False.
+is_student = False
+for_sale = False
+is_online = True
+
